bc4bbbd1a0ff4f4fc3f89f7c05f5c21825bdc36f
Tae Hee Choi <tchoi@nitinol.internal>
2019-07-03 09:17:26 -0700

    __x vs x

--- property.py
+++ property.py
@@ -1,6 +1,6 @@
 class Celsis:
     def __init__(self, temperature=0):
-        self.temperature = temperature
+        self._temperature = temperature
     
     def to_fahrenheit(self):
         return (self.temperature * 1.8) + 32
